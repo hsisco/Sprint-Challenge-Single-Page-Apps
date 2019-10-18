ae91e04fcfaf3b5b1a6e323e042159e59102f23d
hsisco <hysensisco@gmail.com>
2019-10-18 08:52:52 -0700

    Search form structure in App

--- rickmorty/src/App.js
+++ rickmorty/src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import CharacterCard from './components/CharacterCard';
 
 function App() {
   const [character, setCharacter] = useState([]);
   const [search, setSearch] = useState("");
 
+  useEffect
+
   const handleSearch = event => {
     setSearch(event.target.value)
   }
 
-  const searchCharacters = character.filter(person => {
-    person.name() === search.name;
-  })
+  const searchCharacter = character.filter(person => {
+    person.name() === (search.name);
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>"What up my Glip Glops?!"</h1>
       </header>
-
+      <div className="search">
+        <h3>Search Characters by Name</h3>
+        <label>
+          <input
+          type='text'
+          name='searchField'
+          value= {search.name}
+          onChange={handleSearch}
+          />
+        </label>
+        <CharacterCard character={searchCharacter} />
+      </div>
     </div>
   );
 }
