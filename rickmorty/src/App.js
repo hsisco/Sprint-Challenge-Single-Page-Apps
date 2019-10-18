25e30fd2d3f312fde201ae3cad1cf48c0e74c668
hsisco <hysensisco@gmail.com>
2019-10-18 11:20:42 -0700

    changing so many things

--- rickmorty/src/App.js
+++ rickmorty/src/App.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import { Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Jumbotron, Container } from 'reactstrap';
 import './App.css';
-import CardDeck from './components/CardDeck';
+import CharacterCard from './components/CharacterCard';
 
 function App() {
+  const [character, setCharacter] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect (() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(response => {
+      console.log(response.data.results);
+      setCharacter(response.data.results);
+      setSearch(response.data.results)
+    })
+    .catch(error => {
+      console.log("Character data was not returned. Sad.", error);
+    });
+  }, []);
+  
+  const handleSearch = event => {
+    setSearch(event.target.value)
+  }
+
+  const searchCharacter = character.filter(person => {
+    if (person.name === search.name) {
+      return(
+        <CharacterCard character={setCharacter} />
+      );
+    } else {
+      return "Character not found";
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Jumbotron fluid>
+        <Container fluid>
         <h1>"What up my Glip Glops?!"</h1>
-      </header>
-        <CardDeck />
+        </Container>
+      </Jumbotron>
+        <div className="search">
+          <h3>Search Characters by Name</h3>
+          <label>
+            <input
+            type='text'
+            name='searchField'
+            value= {search.name}
+            onChange={handleSearch}
+            />
+          </label>
+          <button type="submit">Search Characters</button>
+        </div>
+      <Route path="/" component={CharacterCard} />
     </div>
   );
 }
--- rickmorty/src/components/CharacterCard.js
+++ rickmorty/src/components/CharacterCard.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
 
 function CharacterCard(props) {
+  return (
+    <div className="card-content">
+      {props.character.map(person => (
+        <CharacterDetails key={person.id} person={person} />
+      ))}
+    </div>
+  );
+}
+
+function CharacterDetails(props) {
+  const { image, name, species, origin, created } = props.person;
+  console.log(props.person);
   return(
     <div>
     <Card>
-      <CardImg top width="100%" src={props.image} alt={`Image of ${props.name} from Rick and Morty.`} />
+      <CardImg top width="100%" src={image} alt={`Image of ${name} from Rick and Morty.`} />
       <CardBody>
-        <CardTitle>{props.name}</CardTitle>
-        <CardText>A {props.species} from {props.origin}.</CardText>
+        <CardTitle>{name}</CardTitle>
+        <CardText>A {species} from {origin}.</CardText>
         <CardText>
-          <small className="text-muted">{props.created}</small>
+          <small className="text-muted">Created: {created}</small>
         </CardText>
       </CardBody>
     </Card>
