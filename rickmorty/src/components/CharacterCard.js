15bdd0a26aa6a20d2a022284dbdb742368e15a8b
hsisco <hysensisco@gmail.com>
2019-10-25 12:06:45 -0700

    Fixed props on CC

--- rickmorty/src/components/CharacterCard.js
+++ rickmorty/src/components/CharacterCard.js
@@ -3,15 +3,15 @@ import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
 
 
 function CharacterCard(props) {
-  const { image, name, species, origin, created } = props.person;
-  console.log(props.person);
+  const { image, name, species, origin, created } = props;
+
   return(
     <div>
     <Card>
-      <CardImg top width="100%" src={image} alt={`Image of ${name} from Rick and Morty.`} />
+      <CardImg top width="20%" src={image} alt={`Image of ${name} from Rick and Morty.`} />
       <CardBody>
         <CardTitle>{name}</CardTitle>
-        <CardText>A {species} from {origin}.</CardText>
+        <CardText>A {species} from {origin.name}.</CardText>
         <CardText>
           <small className="text-muted">Created: {created}</small>
         </CardText>
