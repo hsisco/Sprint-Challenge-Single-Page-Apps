c4aca468fb24bedf885848b3b724d4b2e892a850
hsisco <hysensisco@gmail.com>
2019-10-25 12:07:57 -0700

    Fixed props oin CP

--- rickmorty/src/components/CharactersPage.js
+++ rickmorty/src/components/CharactersPage.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
-import SearchForm from './SearchForm';
 import CharacterCard from './CharacterCard';
 
-function CharacterPage(props){
-  const characters = props.characters;
-
+function CharacterPage(props) {
+  const characters = props.characters || [];
   return (
     <div className="character-page">
-      {characters && characters.map(person => (
-        <CharacterCard key={person.id} person={person} />
+      {characters.map(character => (
+        <CharacterCard key={character.id} {...character} />
       ))}
-
-  <Route render={() => <SearchForm characters={characters} />} />
-  {/* <Route render={(props) => <CharacterCard {...props} characters={characters} />} /> */}
 
 </div>
   );
