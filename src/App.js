819524b59e620da2879a584f70e7281dc104107c
hsisco <hysensisco@gmail.com>
2019-11-15 11:06:51 -0800

    READ.ME answer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import {Header} from "./components/Header.js";
 import {WelcomePage} from './components/WelcomePage';
 
@@ -7,7 +8,7 @@ export const App = () => {
   return (
     <main>
       <Header />
-      <WelcomePage />
+      <Route path="/" render={() => <WelcomePage />}/>
     </main>
   );
 }
